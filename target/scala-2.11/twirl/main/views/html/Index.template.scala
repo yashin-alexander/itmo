f086515ea3cb7a246f00a51b592dad8497eeec85
yashin-alexander <yashin-alexander@rambler.ru>
2017-08-20 02:56:01 +0300

    some handlers added

--- target/scala-2.11/twirl/main/views/html/Index.template.scala
+++ target/scala-2.11/twirl/main/views/html/Index.template.scala
@@ -62,7 +62,7 @@ Seq[Any](format.raw/*1.1*/("""<!DOCTYPE html>
 object Index extends Index_Scope0.Index
               /*
                   -- GENERATED --
-                  DATE: Sat Aug 19 19:08:26 MSK 2017
+                  DATE: Sun Aug 20 00:13:14 MSK 2017
                   SOURCE: /home/alexander/Documents/pip/9lab/app/views/Index.scala.html
                   HASH: 7c60aa78b62bf0b8b69f8bc01aa7fcdf2b290d5d
                   MATRIX: 827->0|1064->211|1078->217|1135->254|1222->314|1237->320|1308->370
