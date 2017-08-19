2e1352c34a26740d0bfb4cb65d034904faf379cb
yashin-alexander <yashin-alexander@rambler.ru>
2017-08-19 22:18:46 +0300

    Int area added

--- au/src/app.js
+++ au/src/app.js
@@ -1,9 +1,21 @@
+import {HttpClient, json} from 'aurelia-fetch-client';
+import {InteractiveArea} from './interactiveArea';
+
 export class App {
   constructor() {
     this.mainInfo = 'This page is for authenticated users only. Welcome!';
     this.todoDescription = '';
-    this.value;
   }
 
+  attached() {
+    var canvas = document.getElementById("interactive-area");
+    window.interactiveArea = new InteractiveArea(
+      0,
+      canvas.getContext("2d"),
+      canvas.width,
+      canvas.height
+    );
+    interactiveArea.drawArea();
+  }
 }
 
--- au/src/main.js
+++ au/src/main.js
@@ -1,4 +1,5 @@
 import environment from './environment';
+import {HttpClient} from 'aurelia-fetch-client';
 
 export function configure(aurelia) {
   aurelia.use
