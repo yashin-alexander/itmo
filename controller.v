ad937a4934398c4a3e80ae6a16be7f1c72cecb9b
yashin-alexander <yashin-alexander@rambler.ru>
2017-09-30 20:18:30 +0300

    mode switch fixed
    Mode switches by posedge clock 1000\500\200\0 times now without #
    command

--- controller.v
+++ controller.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 `define MOD0 0
-`define MOD1 1000
-`define MOD2 500
-`define MOD3 200
+`define MOD1 500
+`define MOD2 250
+`define MOD3 100
 
 module controller( SCLK, SW, LD, mode_clock);
 
@@ -11,6 +11,8 @@ input [1:0] SW;
 output [15:0] LD;
 output mode_clock;
 
+reg [9:0] counter = 0;
+reg [1:0] SW_OLD;
 reg mode_clock = 0;
 
 LED_logic led (
@@ -19,28 +21,41 @@ LED_logic led (
 );
 
 always @(posedge SCLK) begin
+	if (SW_OLD != SW) begin 
+		counter = 0;
+		assign SW_OLD = SW;
+	end 
+		
 	case(SW)
+		0:
+			mode_clock = `MOD0;
 		1:
 			begin
-				# `MOD1;
-				mode_clock = ~mode_clock;
+				if (counter == `MOD1) 
+					begin
+						mode_clock = ~mode_clock;
+						counter = 0;
+					end
+				counter = counter + 1;
 			end
 		2:
 			begin
-				# `MOD2; 
-				mode_clock = ~mode_clock;
+				if (counter == `MOD2) 
+					begin
+						mode_clock = ~mode_clock;
+						counter = 0;
+					end
+				counter = counter + 1;
 			end
 		3:
 			begin
-				# `MOD3; 
-				mode_clock = ~mode_clock;
-			end
-		0:
-			begin
-				# `MOD0; 
-				mode_clock = 0;
+				if (counter == `MOD3) 
+					begin
+						mode_clock = ~mode_clock;
+						counter = 0;
+					end
+				counter = counter + 1;
 			end
 	endcase
 end
-
 endmodule
