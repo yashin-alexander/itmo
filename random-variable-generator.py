3fb18e37a37747dfb1f817c44841744fe6a2b658
yashin-alexander <yashin-alexander@rambler.ru>
2017-10-01 03:31:50 +0300

    exponential distribution added, even-distr code fixed

--- random-variable-generator.py
+++ random-variable-generator.py
@@ -8,29 +8,50 @@ V = 64000
 G = 12800   # left border
 D = 115200  # right border
 E = 12800
-YO = 7      # 3 + 4
+YO = 7
+
+NUMBER_OF_STEPS = 1000
+STEP = round(((D - G) / 1000), 1)
+
+
+def make_numbers_count():
+    return int(sys.argv[1])
+
+
+def make_iteration_list():
+    iteration_list = {}
+    for i in range(0, NUMBER_OF_STEPS+1):
+        iteration_list[i] = round(G + STEP*i, 1)
+    return iteration_list
 
 
 def even_distribution(number):
-    calculation_sheet = {}
+    result_list = {}
+
     for i in range(number):
-        value = random.randint(G, D)        # specify borders for generation
+        value = random.randint(0, NUMBER_OF_STEPS+1)        # specify borders for generation
         try:
-            calculation_sheet[value] = calculation_sheet[value] + 1
+            result_list[value] = result_list[value] + 1
         except:
-            calculation_sheet[value] = 1
-    return calculation_sheet
+            result_list[value] = 1
+
+    return result_list
 
 
-# def exp_distribution(number):
-#     li = {}
-#     for i in range(number):
-#         value = math.floor(random.expovariate(0.5))
-#         try:
-#             li[value] = li[value] + 1
-#         except:
-#             li[value] = 1
-#     return li
+def exp_distribution(number):
+    result_list = {}
+
+    for i in range(number):
+        value = round(random.expovariate(0.000019531))  # 1/(64000-12800)
+        step_number = math.floor(value / STEP)
+
+        try:
+            result_list[step_number] = result_list[step_number] + 1
+        except:
+            result_list[step_number] = 1
+
+    return result_list
+
 #
 #
 # def erlang_distribution(number):
@@ -44,27 +65,33 @@ def even_distribution(number):
 #     return li
 
 
-# def print_results(number, even_result, exp_result, erlang_result):
-def print_results(number, even_result):
+def print_results(iteration_list, even_result_1, even_result_2, exp_result):
 
-    print("\t\t\tEVEN\t\tEXPONENTIAL\t\tERLANG")
-    for i in range(G, D):
-        try:
-            even_number = even_result[i]
-        except:
-            even_number = 0
+    print("  i\t\tITER\t\tEVEN_1\t\tEVEN_2\t\tEXPONENTIAL\t\tERLANG")
+    for i in range(0, NUMBER_OF_STEPS*5+1):
 
-        if even_number != 0:
-            print('i= %(iter)d\t\teven = %(even)d' % {"iter": i, "even": even_number})
+        if i in even_result_1 or i in even_result_2 or i in exp_result:
+            print('%(index)4.d\t%(iter)13.7s\t%(even_1)13.8s\t%(even_2)13.8s\t%(exp)13.8s\t'
+
+                  % {"index": i,
+                     "iter": iteration_list.get(i),
+                     "even_1": even_result_1.get(i),
+                     "even_2": even_result_2.get(i),
+                     "exp": exp_result.get(i)
+                     })
 
 
 def main():
-    even_result = even_distribution(int(sys.argv[1]))
-    # exp_result = exp_distribution(int(sys.argv[1]))
+    random_numbers_count = make_numbers_count()
+    iteration_list = make_iteration_list()
+
+    even_result_1 = even_distribution(random_numbers_count)
+    even_result_2 = even_distribution(random_numbers_count)
+    exp_result = exp_distribution(random_numbers_count)
     # erlang_result = erlang_distribution(int(sys.argv[1]))
 
-    # print_results(int(sys.argv[1]), even_result, exp_result, erlang_result)
-    print_results(int(sys.argv[1]), even_result)
+    print_results(iteration_list, even_result_1, even_result_2, exp_result)
+
 
 if __name__ == "__main__":
     main()
