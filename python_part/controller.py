ef8affbe50c83bd32c1f578dedbde2ab2fe2aeda
yashin-alexander <yashin-alexander@rambler.ru>
2017-11-29 23:00:47 +0300

    modeling: python part constants

---
+++ python_part/controller.py
@@ -0,0 +1,12 @@
+import simpy
+
+env = simpy.Environment()
+
+# scenario variants
+MONSTERS_NUMBER =   ((720, 360, 180, 90), (720, 360, 180, 90), (720, 360, 180, 90), (720, 360, 180, 90))
+TIMEZONES_NUMBER =  ((8, ),               (10, ),              (8, ),               (8, )              )
+VISITING HOURS =    ((8, ),               (10, ),              (8, ),               (3, )              )
+TIMID_CHILDREN =    ((98.92, ),           (98.92, ),           (97, ),              (98.92, )          )
+
+TIMEZONES_POPULATION = {} # todo
+
