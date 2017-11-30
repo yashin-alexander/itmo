a377e801aae8af4081cf0323ba9f6d962c5ad8f6
yashin-alexander <yashin-alexander@rambler.ru>
2017-12-01 00:32:33 +0300

    python_part: doors in-system loading added
    files renaming

--- python_part/controller.py
+++ python_part/controller.py
@@ -1,39 +1,84 @@
 import simpy
+import numpy
+import progressbar
+
+from constants import (MONSTERS_NUMBER,
+                       TIMEZONES_NUMBER,
+                       VISITING_HOURS,
+                       TIMID_CHILDREN,
+                       TIMEZONES_POPULATION,
+                       HOUR,
+                       DURATION_OF_FRIGHT)
+
+queue_length = 0
+
+
+class ChildDoor(object):
+    @staticmethod
+    def run():
+        time = env.now
+        with scenario_one.request() as request:
+                yield request
+                # service_time = numpy.random.uniform(DURATION_OF_FRIGHT[0], DURATION_OF_FRIGHT[1])
+                yield env.timeout(14)
+                wait = env.now
+                print("There {} door processed, time = {}".format(queue_length, wait - time))
+
+
+def source_doors(env):
+    ind = 0
+    while env.now < (HOUR*VISITING_HOURS[0][0]):
+        ind += 1
+        # doors_number = TIMEZONES_POPULATION[env.now/HOUR]
+        doors_number = 20
+        print("{} HOUR is now running".format(env.now/HOUR+1))
+
+        global queue_length
+        queue_length += doors_number
+        print(queue_length)
+        while queue_length > 0:
+            env.process(ChildDoor.run())
+            queue_length -= 1
+        yield env.timeout(HOUR)
+
 
 env = simpy.Environment()
+scenario_one = simpy.Resource(env, capacity=2)
+env.process(source_doors(env))
+env.run(until=HOUR*VISITING_HOURS[0][0])
 
-# scenario variants
-MONSTERS_NUMBER =   ((720, 360, 180, 90), (720, 360, 180, 90), (720, 360, 180, 90), (720, 360, 180, 90))
-TIMEZONES_NUMBER =  ((8, ),               (10, ),              (8, ),               (8, )              )
-VISITING_HOURS =    ((8, ),               (10, ),              (8, ),               (3, )              )
-TIMID_CHILDREN =    ((98.92, ),           (98.92, ),           (97, ),              (98.92, )          )
 
-TIMEZONES_POPULATION = {
-        '-11': 55593,
-        '-10': 1713232,
-        '-9': 741174, # and -9:30
-        '-8': 61526901,
-        '-7': 32872730,
-        '-6': 240604308,
-        '-5': 301108339,
-        '-4': 99579743, # and -3:30
-        '-3': 241618458,
-        '-2': 2784,
-	'-1': 773395,
-	'0': 252535680,
-	'+1': 779333045,
-	'+2': 445080472,
-	'+3': 585168798,
-	'+4': 152492005, # and +3:30 and +4:30
-	'+5': 1601497717, # and +5:30
-	'+6': 293582086, # and +6:30 and +5:45
-	'+7': 398538888,
-	'+8': 1578332933, # and +08:45
-	'+9': 213545890, # and +8:30 and +9:30
-	'+10': 31400559,
-	'+11': 1919388, # and +10:30
-	'+12': 6157999 # and +12:45
-        }
 
-print(sum(TIMEZONES_POPULATION.values()))
 
+
+
+
+
+
+
+
+
+
+# def generate():
+#     bar = progressbar.ProgressBar(widgets=[
+#         ' [', progressbar.Timer(), '] ',
+#         progressbar.AnimatedMarker(markers='<3 '),
+#         progressbar.AnimatedMarker(markers='3 <'),
+#         progressbar.AnimatedMarker(markers=' <3'),
+#         progressbar.AnimatedMarker(markers='<3 '),
+#         progressbar.AnimatedMarker(markers='3 <'),
+#         progressbar.AnimatedMarker(markers=' <3'),
+#         progressbar.AnimatedMarker(markers='<3 '),
+#         progressbar.AnimatedMarker(markers='3 <'),
+#         progressbar.AnimatedMarker(markers=' <3'),
+#
+#         progressbar.Bar(),
+#     ])
+#
+#     for i in range(TIMEZONES_POPULATION["0"]):
+#         interval = HOUR
+#         yield env.timeout(interval)
+#         env.process(Transaction.run())
+#
+#         if i % round((TIMEZONES_POPULATION["0"]/100), 0) == 0:
+#             bar.update(i/(TIMEZONES_POPULATION["0"]/100))
