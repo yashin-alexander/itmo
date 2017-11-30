693f2a1d9e43bb369eb13ab7cf5f397ecb359fb5
yashin-alexander <yashin-alexander@rambler.ru>
2017-11-30 17:10:35 +0300

    python_part: timezones example

--- python_part/controller.py
+++ python_part/controller.py
@@ -5,8 +5,19 @@ env = simpy.Environment()
 # scenario variants
 MONSTERS_NUMBER =   ((720, 360, 180, 90), (720, 360, 180, 90), (720, 360, 180, 90), (720, 360, 180, 90))
 TIMEZONES_NUMBER =  ((8, ),               (10, ),              (8, ),               (8, )              )
-VISITING HOURS =    ((8, ),               (10, ),              (8, ),               (3, )              )
+VISITING_HOURS =    ((8, ),               (10, ),              (8, ),               (3, )              )
 TIMID_CHILDREN =    ((98.92, ),           (98.92, ),           (97, ),              (98.92, )          )
 
-TIMEZONES_POPULATION = {} # todo
+TIMEZONES_POPULATION = {
+        'CentralAmerica7': 61209735,
+        'CentralAmerica8': 7659561,
+        'CentralAmerica9': 25198677,
+        'Alaska10': 186197,
+        'Alaska11': 14142,
+        'Kamchatka12': 2356,
+        'NewZeland11': 2241,
+        'Australia10': 7988302,
+        'Japan9': 47664541,
+        'China8': 351527847  # fix the populations pls
+        }
 
