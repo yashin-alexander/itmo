bbfeae50c93ff85b66a5c5cd806e55d263f2bfeb
yashin-alexander <yashin-alexander@rambler.ru>
2017-12-01 03:32:05 +0300

    terrible shit

--- python_part/controller.py
+++ python_part/controller.py
@@ -1,86 +1,11 @@
-import simpy
-import numpy
-import progressbar
+import runner
 
-from constants import (MONSTERS_NUMBER,
-                       TIMEZONES_NUMBER,
-                       VISITING_HOURS,
-                       TIMID_CHILDREN,
-                       TIMEZONES_POPULATION,
-                       HOUR,
-                       DURATION_OF_FRIGHT)
+worker = runner.Runner()
+worker.execute(0, 0)
+worker = runner.Runner()
+worker.execute(0, 1)
+worker = runner.Runner()
+worker.execute(0, 2)
+worker = runner.Runner()
+worker.execute(0, 3)
 
-queue_length = 0
-monsters_stuck = 0
-doors_stuck = 0
-processed_doors = 0
-
-bar = progressbar.ProgressBar(widgets=[
-    progressbar.Timer(), ' ',
-    progressbar.AnimatedMarker(markers='<3 '),
-    progressbar.AnimatedMarker(markers='3 <'),
-    progressbar.AnimatedMarker(markers=' <3'),
-    progressbar.AnimatedMarker(markers='<3 '),
-    progressbar.AnimatedMarker(markers='3 <'),
-    progressbar.AnimatedMarker(markers=' <3'),
-    progressbar.AnimatedMarker(markers='<3 '),
-    progressbar.AnimatedMarker(markers='3 <'),
-    progressbar.AnimatedMarker(markers=' <3'),
-
-    progressbar.Bar(),
-    progressbar.ETA()
-])
-
-
-class ChildDoor(object):
-    @staticmethod
-    def run():
-        global queue_length, monsters_stuck, doors_stuck, processed_doors
-        queue_length += 1
-        with scenario_one.request() as request:  # SMO 1
-            yield request
-            queue_length -= 1
-
-            if (processed_doors % round((sum/100), 0)) == 0:
-                bar.update(round(processed_doors/sum*100), 0)
-
-            brave = (numpy.random.randint(0, high=100) > TIMID_CHILDREN)
-
-            if not brave:
-                service_time = numpy.random.uniform(DURATION_OF_FRIGHT[0], DURATION_OF_FRIGHT[1])
-                processed_doors += 1
-                yield env.timeout(service_time)
-            else:
-                with brave_child.request() as brave_child_request:  # SMO 2
-                    doors_stuck += 1
-                    yield brave_child_request
-                    doors_stuck -= 1
-                    monsters_stuck += 1
-                    service_time = HOUR*VISITING_HOURS[0][0]
-                    yield env.timeout(service_time)
-
-
-def source_doors(env):
-    ind = 0
-    while env.now < (HOUR*VISITING_HOURS[0][0]):
-        ind += 1
-        doors_number = TIMEZONES_POPULATION[int(env.now/HOUR)]
-
-        for _ in range(doors_number):
-            env.process(ChildDoor.run())
-        yield env.timeout(HOUR)
-
-
-sum = 0
-for i in range(TIMEZONES_NUMBER[0][0]):
-    sum += TIMEZONES_POPULATION[i]
-print(sum)
-
-env = simpy.Environment()
-scenario_one = simpy.Resource(env, capacity=MONSTERS_NUMBER[0][0])
-brave_child = simpy.Resource(env, capacity=MONSTERS_NUMBER[0][0])
-
-env.process(source_doors(env))
-env.run(until=HOUR*VISITING_HOURS[0][0])
-print("\nNot processed doors {}. Monsters stuck {}. Processed doors {}"
-      .format(queue_length + doors_stuck, monsters_stuck, processed_doors))
---
+++ python_part/runner.py
@@ -0,0 +1,86 @@
+import simpy
+import numpy
+import progressbar
+
+from constants import (MONSTERS_NUMBER,
+                       TIMEZONES_NUMBER,
+                       VISITING_HOURS,
+                       TIMID_CHILDREN,
+                       TIMEZONES_POPULATION,
+                       HOUR,
+                       DURATION_OF_FRIGHT)
+
+bar = progressbar.ProgressBar(widgets=[
+    progressbar.Timer(), ' ',
+    progressbar.AnimatedMarker(markers='<3 '),
+    progressbar.AnimatedMarker(markers='3 <'),
+    progressbar.AnimatedMarker(markers=' <3'),
+    progressbar.AnimatedMarker(markers='<3 '),
+    progressbar.AnimatedMarker(markers='3 <'),
+    progressbar.AnimatedMarker(markers=' <3'),
+    progressbar.AnimatedMarker(markers='<3 '),
+    progressbar.AnimatedMarker(markers='3 <'),
+    progressbar.AnimatedMarker(markers=' <3'),
+
+    progressbar.Bar(),
+    progressbar.ETA()
+])
+
+
+class Runner:
+    def __init__(self):
+        self.total_timezones_population = 0
+        self.queue_length = 0
+        self.monsters_stuck = 0
+        self.doors_stuck = 0
+        self.processed_doors = 0
+        self.env = simpy.Environment()
+        self.scenario_num = 0
+        self.experiment_num = 0
+
+    def execute(self, scenario_num, experiment_num):
+        self.scenario_one = simpy.Resource(self.env, capacity=MONSTERS_NUMBER[scenario_num][experiment_num])
+        self.brave_child = simpy.Resource(self.env, capacity=MONSTERS_NUMBER[scenario_num][experiment_num])
+
+        for i in range(TIMEZONES_NUMBER[scenario_num]):
+            self.total_timezones_population += TIMEZONES_POPULATION[i]
+
+        self.env.process(self.source_doors(self.env))
+        self.env.run(until=HOUR*VISITING_HOURS[scenario_num])
+        print("\nNot processed doors {}. Monsters stuck {}. Processed doors {}"
+              .format(self.queue_length + self.doors_stuck, self.monsters_stuck, self.processed_doors))
+
+    def source_doors(self, env):
+        ind = 0
+        while env.now < (HOUR * VISITING_HOURS[self.scenario_num]):
+            ind += 1
+            doors_number = TIMEZONES_POPULATION[int(env.now / HOUR)]
+            # doors_number = 1500
+
+            for _ in range(doors_number):
+                env.process(self.run())
+            yield env.timeout(HOUR)
+
+    def run(self):
+        self.queue_length += 1
+        with self.scenario_one.request() as request:  # SMO 1
+            yield request
+            self.queue_length -= 1
+
+            if (self.processed_doors % round((self.total_timezones_population/100), 0)) == 0:
+                bar.update(round(self.processed_doors / self.total_timezones_population * 100), 0)
+
+            brave = (numpy.random.randint(0, high=100) > TIMID_CHILDREN[self.scenario_num][self.experiment_num])
+
+            if not brave:
+                service_time = numpy.random.uniform(DURATION_OF_FRIGHT[0], DURATION_OF_FRIGHT[1])
+                self.processed_doors += 1
+                yield self.env.timeout(service_time)
+            else:
+                with self.brave_child.request() as brave_child_request:  # SMO 2
+                    self.doors_stuck += 1
+                    yield brave_child_request
+                    self.doors_stuck -= 1
+                    self.monsters_stuck += 1
+                    service_time = HOUR*VISITING_HOURS[self.scenario_num]
+                    yield self.env.timeout(service_time)
