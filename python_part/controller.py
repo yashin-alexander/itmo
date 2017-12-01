7f515b8b31bd08e1042cb8872285d7a345545794
yashin-alexander <yashin-alexander@rambler.ru>
2017-12-01 05:08:50 +0300

    shit

--- python_part/controller.py
+++ python_part/controller.py
@@ -1,5 +1,12 @@
 import runner
 
+from constants import TIMEZONES_POPULATION, TIMEZONES_NUMBER
+
+TIMEZONES_POPULATION.update((x, int(round((y*25.44/100000)+5, 0))) for x, y in TIMEZONES_POPULATION.items())
+
+print(TIMEZONES_POPULATION)
+
+print("\n-----------------------------\n")
 worker = runner.Runner()
 worker.execute(0, 0)
 worker = runner.Runner()
@@ -9,3 +16,32 @@ worker.execute(0, 2)
 worker = runner.Runner()
 worker.execute(0, 3)
 
+print("\n-----------------------------\n")
+worker = runner.Runner()
+worker.execute(1, 0)
+worker = runner.Runner()
+worker.execute(1, 1)
+worker = runner.Runner()
+worker.execute(1, 2)
+worker = runner.Runner()
+worker.execute(1, 3)
+
+print("\n-----------------------------\n")
+worker = runner.Runner()
+worker.execute(2, 0)
+worker = runner.Runner()
+worker.execute(2, 1)
+worker = runner.Runner()
+worker.execute(2, 2)
+worker = runner.Runner()
+worker.execute(2, 3)
+
+print("\n-----------------------------\n")
+worker = runner.Runner()
+worker.execute(3, 0)
+worker = runner.Runner()
+worker.execute(3, 1)
+worker = runner.Runner()
+worker.execute(3, 2)
+worker = runner.Runner()
+worker.execute(3, 3)
--- python_part/runner.py
+++ python_part/runner.py
@@ -7,11 +7,13 @@ from constants import (MONSTERS_NUMBER,
                        VISITING_HOURS,
                        TIMID_CHILDREN,
                        TIMEZONES_POPULATION,
+                       TIME_DELAY,
                        HOUR,
+                       STUCK_TIME,
                        DURATION_OF_FRIGHT)
 
 bar = progressbar.ProgressBar(widgets=[
-    progressbar.Timer(), ' ',
+    progressbar.Timer(),
     progressbar.AnimatedMarker(markers='<3 '),
     progressbar.AnimatedMarker(markers='3 <'),
     progressbar.AnimatedMarker(markers=' <3'),
@@ -22,8 +24,7 @@ bar = progressbar.ProgressBar(widgets=[
     progressbar.AnimatedMarker(markers='3 <'),
     progressbar.AnimatedMarker(markers=' <3'),
 
-    progressbar.Bar(),
-    progressbar.ETA()
+    progressbar.Bar()
 ])
 
 
@@ -43,19 +44,21 @@ class Runner:
         self.brave_child = simpy.Resource(self.env, capacity=MONSTERS_NUMBER[scenario_num][experiment_num])
 
         for i in range(TIMEZONES_NUMBER[scenario_num]):
-            self.total_timezones_population += TIMEZONES_POPULATION[i]
+            self.total_timezones_population += TIMEZONES_POPULATION[i+TIME_DELAY]
 
         self.env.process(self.source_doors(self.env))
         self.env.run(until=HOUR*VISITING_HOURS[scenario_num])
         print("\nNot processed doors {}. Monsters stuck {}. Processed doors {}"
               .format(self.queue_length + self.doors_stuck, self.monsters_stuck, self.processed_doors))
 
+        print("Monster productivity {} %/per monster"
+              .format(self.processed_doors/(self.total_timezones_population*MONSTERS_NUMBER[scenario_num][experiment_num])))
+
     def source_doors(self, env):
         ind = 0
         while env.now < (HOUR * VISITING_HOURS[self.scenario_num]):
             ind += 1
             doors_number = TIMEZONES_POPULATION[int(env.now / HOUR)]
-            # doors_number = 1500
 
             for _ in range(doors_number):
                 env.process(self.run())
@@ -82,5 +85,5 @@ class Runner:
                     yield brave_child_request
                     self.doors_stuck -= 1
                     self.monsters_stuck += 1
-                    service_time = HOUR*VISITING_HOURS[self.scenario_num]
+                    service_time = STUCK_TIME
                     yield self.env.timeout(service_time)
