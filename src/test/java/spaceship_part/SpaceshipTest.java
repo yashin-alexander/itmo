9b99437fb44f2e600cf6300ac177ff4569b80694
yashin-alexander <yashin-alexander@rambler.ru>
2018-03-22 15:49:03 +0300

    add uml.png

---
+++ src/test/java/spaceship_part/SpaceshipTest.java
@@ -0,0 +1,14 @@
+package spaceship_part
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SpaceshipTest {
+
+    @BeforeClass
+    public void setup(){
+
+    }
+}
+
