4588557dc233a69476c3578e260dbf176c03e104
yashin-alexander <alexandr.yashin@emlid.com>
2018-05-02 14:42:34 +0300

    databases/mongo: restaurants -> places

--- databases/mongo/create_db.js
+++ databases/mongo/create_db.js
@@ -41,12 +41,12 @@ var UserSchema = new Schema({
     },
     profession: String,
     reservations: [ {
-	place: {type: Schema.Types.ObjectId, ref: 'Restaurant'},
+	place: {type: Schema.Types.ObjectId, ref: 'Place'},
 	time: Date,
 	number_of_persons: Number
     } ],
     ordering_food: [ {
-	place: {type: Schema.Types.ObjectId, ref: 'Restaurant'},
+	place: {type: Schema.Types.ObjectId, ref: 'Place'},
 	time: Date,
 	address: String,
 	dish: String
@@ -59,31 +59,31 @@ var UserSchema = new Schema({
     } ]
 });
 var User = mongoose.model('User', UserSchema);
+
+/*-----------------reservations-----------------*/
  
-/*-----------------restaurants-----------------*/
- 
-var RestaurantSchema = new Schema({
+var PlaceSchema = new Schema({
     name: String,
     address: String,
     phone: String,
     rating: Number
 });
-var Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+var Place = mongoose.model('Place', PlaceSchema);
  
 /*-------------------inserts-------------------*/
 
-var restaurant = new Restaurant();
-restaurant.name = "dodo-pizza";
-restaurant.address = "moskovskaya, 16";
-restaurant.phone = "9602603026";
-restaurant.rating = 4.5;
-restaurant.save();
+var place = new Place();
+place.name = "dodo-pizza";
+place.address = "moskovskaya, 16";
+place.phone = "9602603026";
+place.rating = 4.5;
+place.save();
 
 var user = new User();
 user.name = 'sonya';
 user.age = 21;
 user.profession = "president";
-user.reservations.push({ place: restaurant._id, time: new Date(2017, 10, 10), number_of_persons: 3});
-user.reservations.push({ place: restaurant._id, time: new Date(2018, 11, 11), number_of_persons: 20});
+user.reservations.push({ place: place._id, time: new Date(2017, 10, 10), number_of_persons: 3});
+user.reservations.push({ place: place._id, time: new Date(2018, 11, 11), number_of_persons: 20});
 user.save();
 
