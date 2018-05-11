fc3b14de0f65f38836f79f5c3e140c5d68578c77
yashin-alexander <alexandr.yashin@emlid.com>
2018-05-12 01:49:18 +0300

    api/global_api: add some neoj calls

--- api/global_api.py
+++ api/global_api.py
@@ -2,6 +2,7 @@ import flask
 from werkzeug.routing import BaseConverter
 
 from mongo_api.api import MongoAPI
+from neo4j_api.api import Neo4jAPI
 from cassandra_api.api import CassandraAPI
 
 
@@ -15,6 +16,7 @@ app = flask.Flask(__name__)
 app.url_map.converters['regex'] = RegexConverter
 
 mongo_api = MongoAPI()
+neo4j_api = Neo4jAPI()
 try:
     cassandra_api = CassandraAPI()
 except Exception:
@@ -131,6 +133,26 @@ def cassandra_delete_enter_attempts():
     return cassandra_api.delete_enter_attempts()
 
 # NEO4J API CALLS
+
+
+@app.route('/neo4j/create_history', methods=['POST'])
+def neo4j_create_history():
+    return neo4j_api.create_history()
+
+
+@app.route('/neo4j/history', methods=['GET'])
+def neo4j_history():
+    return neo4j_api.history()
+
+
+@app.route('/neo4j/update_history', methods=['POST'])
+def neo4j_update_history():
+    return neo4j_api.update_history()
+
+
+@app.route('/neo4j/delete_history', methods=['GET'])
+def neo4j_delete_history():
+    return neo4j_api.delete_history()
 
 
 if __name__ == '__main__':
