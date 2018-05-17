7cf890e53616495288c8804fb1104a6cd8c0575b
yashin-alexander <alexandr.yashin@emlid.com>
2018-05-17 15:52:45 +0300

    add add_new_repository script

---
+++ add_new_repository.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# $1 - remote project name
+# $2 - remote project repository
+# $3 - `course` directory
+
+git remote add -f $1 $2
+git merge -s ours --allow-unrelated-histories --no-commit $1/master
+git read-tree --prefix=$3/$1 -u $1/master
+git commit -m "Merge $1 as itmo/$1 subdirectory"
+git pull -s subtree $1 master
+
