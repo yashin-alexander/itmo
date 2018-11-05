251a66cc52300c80e6f6a0798fe4b708b59ce7c6
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-05 23:37:39 +0300

    main.c: app adapted for nucleo-f091rc
    - timeout increased

--- main.c
+++ main.c
@@ -1,48 +1,37 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
-
-#include "stm32f30x.h"
-#include "stm32f30x_gpio.h"
+#include "stm32f0xx.h"
+#include "stm32f0xx_gpio.h"
 
 typedef uint8_t (*check_cancellation_req_fn) (void);
 
 void delay(void) {
-	int i = 1000000;
-	while(i--);
+    int i = 2000000;
+    while(i--);
 }
 
 
 uint8_t user_button_pressed(void)
 {
-	return GPIO_ReadInputData(GPIOD) & GPIO_Pin_2;
+    return (GPIO_ReadInputData(GPIOD) & GPIO_Pin_2);
 }
 
 
 uint8_t user_button_released(void)
 {
-	return !user_button_pressed();
+    return !user_button_pressed();
 }
 
 
 static
 void gpio_toggle(GPIO_TypeDef* gpio_port, uint32_t gpio_pins)
 {
-	gpio_port->ODR ^= gpio_pins;
+    gpio_port->ODR ^= gpio_pins;
 }
 
 
 static
 void gpio_configure_output(GPIO_TypeDef* gpio_port, uint16_t gpio_pin)
 {
-	GPIO_InitTypeDef gpio_init = {0};
+    GPIO_InitTypeDef gpio_init = {0};
     gpio_init.GPIO_Pin = gpio_pin;
     gpio_init.GPIO_Speed = GPIO_Speed_Level_3;
     gpio_init.GPIO_Mode = GPIO_Mode_OUT;
@@ -53,100 +42,69 @@ void gpio_configure_output(GPIO_TypeDef* gpio_port, uint16_t gpio_pin)
 static
 void gpio_configure_input(GPIO_TypeDef* gpio_port, uint16_t gpio_pin)
 {
-	GPIO_InitTypeDef gpio_init = {0};
-	gpio_init.GPIO_Pin = gpio_pin;
-	gpio_init.GPIO_Speed = GPIO_Speed_Level_3;
-	gpio_init.GPIO_Mode = GPIO_Mode_IN;
-	GPIO_Init(gpio_port, &gpio_init);
+    GPIO_InitTypeDef gpio_init = {0};
+    gpio_init.GPIO_Pin = gpio_pin;
+    gpio_init.GPIO_Speed = GPIO_Speed_Level_3;
+    gpio_init.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_Init(gpio_port, &gpio_init);
 }
 
 
 static
 void shift_animation_init(GPIO_TypeDef* gpio_port, uint32_t* pins, uint32_t pins_size)
 {
-	uint32_t initial_pins = pins[0] | pins[1] | pins[2];
-	gpio_toggle(GPIOC, initial_pins);
+    uint32_t initial_pins = pins[0] | pins[1] | pins[2];
+    gpio_toggle(GPIOC, initial_pins);
 }
 
 
 static
 void shift_right_animation(GPIO_TypeDef* gpio_port,
-		                   check_cancellation_req_fn cancel_requested,
-		                   uint32_t* pins,
-						   uint32_t pins_size)
+                           check_cancellation_req_fn cancel_requested,
+                           uint32_t* pins,
+                           uint32_t pins_size)
 {
-	for (uint8_t i = 3; i < pins_size ; i++) {
-		delay();
-		if (cancel_requested()) { return; }
-		gpio_toggle(gpio_port, pins[i] | pins[i - 3]);
-	}
+    for (uint8_t i = 0; i < pins_size ; i++) {
+        delay();
+        if (cancel_requested()) { return; }
+            gpio_toggle(gpio_port, pins[i]);
+            if (i == 0)
+                gpio_toggle(gpio_port, pins[pins_size - 1]);
+            else
+                gpio_toggle(gpio_port, pins[i - 1]);
+    }
 }
 
-
-static
-void shift_left_animation(GPIO_TypeDef* gpio_port,
-		                  check_cancellation_req_fn cancel_requested,
-		                  uint32_t* pins,
-						  uint32_t pins_size)
-{
-	for (uint8_t i = pins_size - 1; i >= 3; i--) {
-		delay();
-		if (cancel_requested()) { return; }
-		gpio_toggle(gpio_port, pins[i] | pins[i - 3]);
-	}
-}
 
 
 static
 void accumulation_animation_init(GPIO_TypeDef* gpio_port, uint32_t* pins, uint32_t pins_size)
 {
-	uint8_t last_idx = pins_size - 1;
-	uint32_t initial_pins = pins[0] | pins[last_idx];
-	gpio_toggle(gpio_port, initial_pins);
+    uint8_t last_idx = pins_size - 1;
+    uint32_t initial_pins = pins[0] | pins[last_idx];
+    gpio_toggle(gpio_port, initial_pins);
 }
 
 
 static
 void accumulation_animation(GPIO_TypeDef* gpio_port,
-		                    check_cancellation_req_fn cancel_requested,
-		                    uint32_t* pins,
-							uint32_t pins_size)
+                            check_cancellation_req_fn cancel_requested,
+                            uint32_t* pins,
+                            uint32_t pins_size)
 {
-	uint8_t frames_count = pins_size / 2 - 1;
-	uint8_t last_idx = pins_size - 1;
+    uint8_t frames_count = pins_size / 2 - 1;
+    uint8_t last_idx = pins_size - 1;
 
-	for (uint8_t i = 1; i <= frames_count; i++) {
-		delay();
-		if (cancel_requested()) { return; }
-		uint32_t pins_for_toggle =
-				pins[i - 1] |
-				pins[i] |
-				pins[last_idx - i + 1] |
-				pins[last_idx -i];
-		gpio_toggle(gpio_port, pins_for_toggle);
-	}
-}
-
-
-static
-void dispersion_animation(GPIO_TypeDef* gpio_port,
-		                  check_cancellation_req_fn cancel_requested,
-		                  uint32_t* pins,
-						  uint32_t pins_size)
-{
-	uint8_t frames_count = pins_size / 2 - 1;
-	uint8_t last_idx = pins_size - 1;
-
-	for (uint8_t i = frames_count; i >= 1; i--) {
-		delay();
-		if (cancel_requested()) { return; }
-		uint32_t pins_for_toggle =
-			    pins[i - 1] |
-				pins[i] |
-				pins[last_idx - i + 1] |
-				pins[last_idx - i];
-		gpio_toggle(gpio_port, pins_for_toggle);
-	}
+    for (uint8_t i = 1; i <= frames_count; i++) {
+        delay();
+        if (cancel_requested()) { return; }
+        uint32_t pins_for_toggle =
+                pins[i - 1] |
+                pins[i] |
+                pins[last_idx - i + 1] |
+                pins[last_idx -i];
+        gpio_toggle(gpio_port, pins_for_toggle);
+    }
 }
 
 
@@ -157,56 +115,43 @@ int main(void)
     delay();
 
     uint32_t pins_map[] = {
-    		GPIO_Pin_0,
-			GPIO_Pin_1,
-			GPIO_Pin_2,
-			GPIO_Pin_3,
-			GPIO_Pin_5,
-			GPIO_Pin_6,
-			GPIO_Pin_7,
-			GPIO_Pin_8
+            GPIO_Pin_5,
+            GPIO_Pin_8,
+            GPIO_Pin_6,
+            GPIO_Pin_9,
     };
-    uint32_t pins_map_size = 8;
+    uint32_t pins_map_size = 4;
 
     // for animation (PC4 is busy)
     for (uint8_t i = 0; i < pins_map_size; i++) {
-    	gpio_configure_output(GPIOC, pins_map[i]);
+        gpio_configure_output(GPIOC, pins_map[i]);
     }
 
     // for user button
     gpio_configure_input(GPIOD, GPIO_Pin_2);
 
-	for(;;) {
-		if (user_button_pressed()) {
-		    GPIOC->ODR = 0;
-		    accumulation_animation_init(GPIOC, pins_map, pins_map_size);
+    for(;;) {
+        if (user_button_pressed()) {
+            GPIOC->ODR = 0;
+            accumulation_animation_init(GPIOC, pins_map, pins_map_size);
 
-			while (user_button_pressed()) {
-				accumulation_animation(GPIOC,
-							           &user_button_released,
-							           pins_map,
-									   pins_map_size);
+            while (user_button_pressed()) {
+                accumulation_animation(GPIOC,
+                                       &user_button_released,
+                                       pins_map,
+                                       pins_map_size);
+            }
+        } else {
+            GPIOC->ODR = 0;
+            shift_animation_init(GPIOC, pins_map, pins_map_size);
 
-				dispersion_animation(GPIOC,
-							         &user_button_released,
-							         pins_map,
-								     pins_map_size);
-			}
-		} else {
-			GPIOC->ODR = 0;
-			shift_animation_init(GPIOC, pins_map, pins_map_size);
-
-			while (user_button_released()) {
-				shift_right_animation(GPIOC,
-							          &user_button_pressed,
-							          pins_map,
-									  pins_map_size);
-
-				shift_left_animation(GPIOC,
-							         &user_button_pressed,
-							         pins_map,
-									 pins_map_size);
-			}
-		}
-	}
+            while (user_button_released()) {
+                shift_right_animation(GPIOC,
+                                      &user_button_pressed,
+                                      pins_map,
+                                      pins_map_size);
+            }
+        }
+    }
 }
+
